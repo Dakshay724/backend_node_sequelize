0e664d604d8bca3711cc669aa7e4d0c813f00698
akshayd-nimblechapps <akshayd@nimblechapps.in>
2024-08-14 23:45:57 +0530

    new commit

--- source/route.ts
+++ source/route.ts
@@ -2,18 +2,18 @@
 import { Express } from "express";
 import roleRoutes from "./module/role/roleRoute";
 import userRoutes from "./module/user/userRoute";
-import categoryRoutes from "./routes/categoryRoutes";
-import productRoutes from "./routes/productRoutes";
-import stockRoutes from "./routes/stockRoutes";
-import cartRoutes from "./routes/cartRoutes";
-import orderRoutes from "./routes/orderRoutes";
+import categoryRoutes from "./module/category/categoryRoute";
+import productRoutes from "./module/product/productRoute";
+// import stockRoutes from "./module/stock/stockRoute";
+// import cartRoutes from "./module/cart/cartRoute";
+// import orderRoutes from "./routes/orderRoutes";
 
 export default (app: Express) => {
   app.use("/api/roles", roleRoutes);
   app.use("/api/users", userRoutes);
   app.use("/api/categories", categoryRoutes);
   app.use("/api/products", productRoutes);
-  app.use("/api/stocks", stockRoutes);
-  app.use("/api/carts", cartRoutes);
-  app.use("/api/orders", orderRoutes);
+  // app.use("/api/stocks", stockRoutes);
+  // app.use("/api/carts", cartRoutes);
+  // app.use("/api/orders", orderRoutes);
 };
